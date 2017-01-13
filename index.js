892e2c64845678fe0aa95fbb72febe0e2bd857e5
RajNirmal <nirmal497@gmail.com>
2017-01-13 22:45:34 +0530

    Finding Errror

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/push',function(req,res){
   };
   fcm.send(message, function(err, response){
     if (err) {
-        console.log("Something has gone wrong!");
+        console.log("Something has gone wrong!"+err);
     } else {
         console.log("Successfully sent with response: ", response);
     }
