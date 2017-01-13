12b1e9948c23d6703580ebd56d815233c6352124
RajNirmal <nirmal497@gmail.com>
2017-01-13 07:44:35 +0530

    not working

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.get('/hello', function(request, response){
   var StringedJson = "{name : "+ outputJson +"}";
   //response.send(JSON.stringify(StringedJson));
   response.JSON(StringedJson)*/
-  String sJson = "'Sent from'  : 'heroku server'";
-  response.send(JSON.stringify(sJson));
+  String myJson = "'Sent from'  : 'heroku server'";
+  response.send(JSON.stringify(myJson));
 })
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
