5ebba3cf2c550b2115f44cb25f65ab1f37a0cacb
RajNirmal <nirmal497@gmail.com>
2017-01-16 11:14:44 +0530

    Revoed a semi

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var fcm = new FCM('legacyKey');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended : true;
+  extended : true
 }))
 app.set('port', (process.env.PORT || 5000));
 
